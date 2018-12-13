5461704649aee1564ce4e4789f2b761e6fac4af3
YongHun <sabgilhun@gmail.com>
2018-12-13 12:40:31 +0900

    MovieSearcher 삭제, 기능 Searcher로 통합

--- app/src/main/java/org/sabgil/moviereviewer/model/Searcher.java
+++ app/src/main/java/org/sabgil/moviereviewer/model/Searcher.java
@@ -2,85 +2,84 @@ package org.sabgil.moviereviewer.model;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
 
 public class Searcher {
     private final static String TAG = Searcher.class.getSimpleName();
 
-    String clientId;
-    String clientSecret;
-    String apiUrl;
-    String resultXML;
-    protected String additionParam = "";
+    public String clientId;
+    public String clientSecret;
+    public String apiUrl;
+    protected HashMap<String, String> requestParam;
 
-    public Searcher() {
+    public Searcher(String id, String secret, String url) {
+        clientId = id;
+        clientSecret = secret;
+        apiUrl = url;
+        requestParam = new HashMap<>();
     }
 
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
+    private String convertParamToUrl() {
+        String param = "";
+
+        if (requestParam.isEmpty())
+            return "";
+
+        for (String key : requestParam.keySet()) {
+            param += ("&" + key + "=" + requestParam.get(key));
+        }
+
+        return param;
     }
 
-    public void setClientSecret(String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
+    private String makeUrl(String searchWord) {
+        String url;
 
-    public void setApiUrl(String apiUrl) {
-        this.apiUrl = apiUrl + "?query=";
+        try {
+            url = apiUrl + "?query=" + URLEncoder.encode(searchWord, "UTF-8");
+            Log.i(TAG, url);
+        } catch (Exception e) {
+            Log.e(TAG, "encoding error");
+            return null;
+        }
+
+        return url;
     }
 
     public void clearParam() {
-        additionParam = "";
+        requestParam.clear();
     }
 
-    public void search(final String method, String text) {
-        final String searchUrl;
+    public InputStream search(String searchWord) {
+        String searchUrl;
+        InputStream resultStream;
+        HttpURLConnection connection;
 
+        searchUrl = makeUrl(searchWord) + convertParamToUrl();
         try {
-            searchUrl = apiUrl + URLEncoder.encode(text, "UTF-8") + additionParam;
-            Log.i(TAG, searchUrl);
+            URL url = new URL(searchUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-Naver-Client-Id", clientId);
+            connection.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == 200) {
+                resultStream = connection.getInputStream();
+            } else {
+                resultStream = connection.getErrorStream();
+            }
+            Log.i(TAG, resultStream.toString());
+
         } catch (Exception e) {
-            Log.e(TAG, "encoding error");
-            return;
+            resultStream = null;
+            Log.e(TAG, "search error", e);
         }
 
-        new Thread() {
-
-            @Override
-            public void run() {
-                super.run();
-                try {
-                    URL url = new URL(searchUrl);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod(method);
-                    con.setRequestProperty("X-Naver-Client-Id", clientId);
-                    con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-                    int responseCode = con.getResponseCode();
-
-                    BufferedReader br;
-                    if (responseCode == 200) {
-                        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    } else {
-                        br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-                    }
-
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-                    while ((inputLine = br.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-
-                    br.close();
-                    resultXML = response.toString();
-
-                    Log.i(TAG, resultXML);
-                } catch (Exception e) {
-                    Log.e(TAG, "search error", e);
-                }
-            }
-        }.start();
+        return resultStream;
     }
 }
